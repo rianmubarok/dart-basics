60ab25e85fbebbf04e2af8dc9c0a044d04f2c492
Rian <fitriyan606@gmail.com>
2026-03-10 22:20:22 +0700

    feat: 1.3 membuat program konstanta

---
+++ dart_project/bin/1.3_konstanta.dart
@@ -0,0 +1,9 @@
+void main() {
+  const int hariDalamSeminggu = 7;
+
+  print('Jumlah hari dalam seminggu: $hariDalamSeminggu');
+
+  int minggu = 4;
+  int totalHari = hariDalamSeminggu * minggu;
+  print('\nJika ada $minggu minggu, maka total hari adalah $totalHari hari.');
+}
